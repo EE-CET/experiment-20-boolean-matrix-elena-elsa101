e28fb843172ae68a325cf83eaf29a63d007355b5
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-28 22:57:49 +0530

    Update BooleanMatrix.java

--- src/BooleanMatrix.java
+++ src/BooleanMatrix.java
@@ -1,14 +1,37 @@
+ import java.util.*;
 public class BooleanMatrix {
-    
-    // TODO: Implement the function to modify the matrix
-    
-        // Logic: Check each row. If it contains a 1, set the whole row to 1.
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt(),n=sc.nextInt();
+int[][] a=new int[m][n];
+for(int i=0;i<m;i++)
+for(int j=0;j<n;j++)
+a[i][j]=sc.nextInt();
+
+for(int i=0;i<m;i++){
+boolean hasOne=false;
+for(int j=0;j<n;j++){
+if(a[i][j]==1){
+hasOne=true;
+break;
+}
+}
+if(hasOne){
+for(int j=0;j<n;j++){
+a[i][j]=1;
+}
+}
+}
+
+for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+System.out.print(a[i][j]);
+if(j<n-1) System.out.print(" ");
+}
+if(i<m-1) System.out.println();
+}
+}
+}
     
 
-    
-        // TODO: Read m and n
-        // TODO: Read the matrix elements
-        // TODO: Call BooleanMatrixProblem(mat, m, n)
-        // TODO: Print the modified matrix
-    
-}
